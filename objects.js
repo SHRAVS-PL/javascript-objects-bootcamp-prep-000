46ff3cf3121585782909bdc7eeb1f3b5fbc39b98
Shravani PL <pl.shravani@gmail.com>
2018-06-01 18:01:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artist:name};
 
 function updatePlaylist(playlist,artistName,songTitle) {
-  Object.assign({},playlist,{artistName:songTitle})
+  Object.assign({},{artistName:songTitle})
   return playlist
 }
